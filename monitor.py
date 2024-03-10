3a390c550fbabb7c94f5d273562af92164aefa6b
Matteo Frigo <114284526+FrigoMatteo@users.noreply.github.com>
2024-03-10 16:55:26 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,5 +1,10 @@
 import requests
 import time
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch, RemoteController
+from mininet.link import TCLink
+from mininet.cli import CLI
 
 #questo script serve per definire la topologia della rete ricevendo dal controller queste informazioni 
 
@@ -34,29 +39,127 @@ def get_hosts():
     else:   
         return None
 
+
+
+class NetworkTopo(Topo):
+    def __init__(self,hosts,switches,links):
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Create template host, switch, and link
+        host_config = dict(inNamespace=True)
+        
+        link_config = dict(bw=1)
+        
+    
+
+topos = {"networkslicingtopo": (lambda: NetworkTopo())}
+
+def create_network(hosts,switches,links):
+    print("\nCreate new network")
+    topo = NetworkTopo(hosts,switches,links)
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink,
+    )
+    net.build()
+    net.start()
+    CLI(net)
+    net.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Esempio di utilizzo delle funzioni per ottenere le informazioni sulla topologia
 if __name__ == "__main__":
     while True:
-        print("START MONITORING!")
+        print("\n\nSTART MONITORING!")
+        new_links=[]
+        new_hosts=[]
+        new_switches=[]
+        
+        
+        #We add the switches to our new network
         switches = get_switches()
+        
         if switches:
-            print("Switches:")
             for switch in switches:
-                print(switch)
-                
+                dicto={"name":"s%d" % int(switch['dpid']), "dpid":"%016x" % int(switch['dpid'])}
+                new_switches.append(dicto)
+                            
         
         
-        links = get_links()
-        if links:
-            print("\nLinks:")
-            for link in links:
-                print(link)
+        #We get the links from the hosts
+        hosts=get_hosts()
         
-        hosts = get_hosts()
         if hosts:
-            print("\nHosts:")
             for host in hosts:
+                mac_parts=host['mac'].split(":")
+                first=0
+                for part in mac_parts:
+                    first = (first << 8) + int(part, 16)
+                
+                new_hosts.append("h%d" % first)
+                
+                dicto={"first":"h%d" % first, "second":"s%d" % int(host['port']['dpid'])}
+                new_links.append(dicto)
+                #print(str(index)+". [Mac:"+host['mac']+"  port:{"+"dpid:'"+host['port']['dpid']+"' name:"+host['port']['name']+"]")
+        
+        
+        
+        #We get the links from the switches
+        links=get_links()
+        
+        if links:
+            for link in links:
+                 dicto={"first":"s%d" % int(link['src']['dpid']), "second":"s%d" % int(link['dst']['dpid'])}
+                 new_links.append(dicto)
+        
+             
+        
+        #We check for duplicates: 
+        # Such as: [{first:"s1", second:"s2"},{first:"s2",second:"s1"}] 
+        for link in new_links:
+            first=link['first']
+            second=link['second']
+            for index,secLink in enumerate(new_links):
+                if secLink['first']==second and secLink['second']==first:
+                    del new_links[index]
+        
+        
+        #Print all devices and links we see from our network
+        if new_hosts:
+            print("\nHosts:")
+            for host in new_hosts:
                 print(host)
-        time.sleep(20)
-
-
+        
+        if new_switches:
+            print("\nSwitches:")
+            for switch in new_switches:
+                print(switch)
+        
+        if new_links:
+            print("\nLinks:")
+            for links in new_links:
+                print(links)  
+        
+        #create_network(new_hosts,new_switches,new_links)
+               
+        time.sleep(10) 
